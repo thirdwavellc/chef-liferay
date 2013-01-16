9e3912fdc5d681d25b1320621b11fb403d4869da
Adam Krone <krone.adam@gmail.com>
2013-01-16 16:31:53 -0600

    Fixes setenv.sh template

--- recipes/default.rb
+++ recipes/default.rb
@@ -47,11 +47,9 @@ file "#{node['liferay']['install_directory']}/liferay/tomcat/bin/*.bat" do
 	action :delete
 end
 
-bash "Optimize memory setting" do
-	code <<-EOH
-	sudo sed -i "1c JAVA_OPTS=\\"\\$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Dcompany-id-properties=true -Xms1024m -Xmx1024m -XX:MaxPermSize=512m\\"" /opt/liferay/tomcat/bin/setenv.sh
-	EOH
-	action :run
+template "#{node['liferay']['install_directory']}/liferay/tomcat/bin/setenv.sh" do
+	source "setenv.sh"
+	mode 00755
 end
 
 directory "#{node['liferay']['install_directory']}/liferay/tomcat/webapps/welcome-theme" do
