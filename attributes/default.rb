6cadfad9f39628113b4aff850b2529c9116776f5
Adam Krone <krone.adam@gmail.com>
2013-01-18 15:45:31 -0600

    Adds Enterprise recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -38,3 +38,7 @@ default['liferay']['extract_command'] = "sudo unzip #{liferay['download_director
 default['liferay']['tomcat_version'] = "tomcat-7.0.27"
 
 default['liferay']['start_command'] = "sudo #{liferay['install_directory']}/liferay/tomcat/bin/startup.sh"
+
+default['liferay']['ee']['license_filename'] = "license-portaldevelopment-developer-cluster-6.1ga2-thirdwavepartnerdevlicense.xml"
+
+default['liferay']['ee']['license_url'] = "http://files.thirdwavellc.com/software/liferay/portal/6.1/bundles/license-portaldevelopment-developer-cluster-6.1ga2-thirdwavepartnerdevlicense.xml"
