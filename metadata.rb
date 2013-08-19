1cffa154fcfb10c72628aafb085f67aec90bd284
Adam Krone <krone.adam@gmail.com>
2013-08-19 11:20:43 -0500

    Increment version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Thirdwave, LLC"
 maintainer_email  "adam.krone@thirdwavellc.com"
 license           "Apache 2.0"
 description       "Installs and configures Liferay"
-version           "0.9.7"
+version           "0.10.0"
 
 depends "apt"
 depends "unzip"
