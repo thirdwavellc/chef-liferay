0e124a2925e152de59e27a3ee92cf7496dc046a2
Orin <orin.fink@thirdwavellc.com>
2014-11-23 09:16:19 -0600

    updates version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Thirdwave, LLC"
 maintainer_email  "adam.krone@thirdwavellc.com"
 license           "Apache 2.0"
 description       "Installs and configures Liferay"
-version           "0.13.2"
+version           "0.13.3"
 
 depends "apt"
 depends "unzip"
